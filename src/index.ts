ce219f30170c50fe22c50c7def39524fb8bcaead
Erick Machado <erick.gsmachado@gmail.com>
2021-11-02 00:17:55 -0300

    feat: create express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import 'dotenv/config'
+import express from 'express'
+import logger from '@/utils/logger'
 
+const server = express()
+
+server.listen(process.env.PORT, () => {
+  logger.info('Server running 🔥')
+})
